217f2163134e71ba609a7c1389f75b52d4ce1025
abdelrahamn-shanan <01012617633abdoshanan@gmail.com>
2023-08-21 14:14:04 +0300

    .

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->integer('order')->unsigned()->default(0);
             $table->timestamps();
         });
     }
